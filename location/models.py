5db368320838db2044b1b48f4212f3cd2926dee4
dreamxinxcode <brandon@dreamincode.dev>
2023-11-23 12:46:36 -0800

    Device model with last_location property and add_location() method

--- location/models.py
+++ location/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Device(models.Model):
+    locations = models.JSONField(default=list)
+    last_update = models.DateTimeField(blank=True, null=True)
+
+    @property
+    def last_location(self):
+        return self.locations[-1] if self.locations else None
+
+    def add_location(self, lon: float, lat: float, alt: float):
+        new_location = {'lon': lon, 'lat': lat, 'alt': alt}
+
+        self.locations.append(new_location)
+        self.last_update = timezone.now()
+
+        # Remove the oldest entry if the number of entries exceeds 5.
+        if len(self.locations) > 5:
+            self.locations.pop(0)
+
+        self.save()
+
+    def __str__(self) -> str:
+        return f'Device: {self.id}'
