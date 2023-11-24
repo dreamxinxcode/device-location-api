ba905e5cb02b77e34f6cb9580864ce73e428f968
dreamxinxcode <brandon@dreamincode.dev>
2023-11-23 22:37:40 -0800

    Cleanup views

--- location/views.py
+++ location/views.py
@@ -1,19 +1,12 @@
 import logging
-
-from rest_framework.views import APIView
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
-
 from .models import Device
 from.serializers import DeviceSerializer
 
 LOGGER = logging.getLogger(__name__)
-
-
-# Feel free to modify this are rebuild the entire API endpoint as needed.
-# Below is just some boilerplate code to help get started
 
 class DeviceView(viewsets.ViewSet):
     serializer = DeviceSerializer
@@ -34,6 +27,11 @@ class DeviceView(viewsets.ViewSet):
         LOGGER.info(f"New update {request} Payload: {request.data}")
         return Response("Update successful!", status=status.HTTP_200_OK)
     
+    def destroy(self, request, pk=None):
+        device = get_object_or_404(Device, id=pk)
+        device.delete()
+        return Response("Device deleted successfully", status=status.HTTP_204_NO_CONTENT)
+
     @action(detail=True, methods=['GET'])
     def last_location(self, request, pk: int=None):
         device = get_object_or_404(Device, id=pk)
