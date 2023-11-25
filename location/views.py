c812a5bd642738941f0bbd0f7554a1ac78ebaf04
dreamxinxcode <brandon@dreamincode.dev>
2023-11-25 00:03:34 -0800

    Add error handling

--- location/views.py
+++ location/views.py
@@ -12,29 +12,49 @@ class DeviceView(viewsets.ViewSet):
     serializer = DeviceSerializer
     
     def list(self, request):
-        queryset = Device.objects.all()
-        serializer = self.serializer(queryset, many=True)
-        return Response(serializer.data)
+        try:
+            queryset = Device.objects.all()
+            serializer = self.serializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            LOGGER.error(f"Error in listing devices: {e}")
+            return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def retrieve(self, request, pk: int=None):
-        device = get_object_or_404(Device, id=pk)
-        serializer = self.serializer(device)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        try:
+            device = get_object_or_404(Device, id=pk)
+            serializer = self.serializer(device)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            LOGGER.error(f"Error in retrieving device: {e}")
+            return Response("Device not found", status=status.HTTP_404_NOT_FOUND)
 
     def update(self, request, pk: int=None):
-        device, _ = Device.objects.get_or_create(id=pk)
-        device.add_location(**request.data)
-        LOGGER.info(f"New update {request} Payload: {request.data}")
-        return Response("Update successful!", status=status.HTTP_200_OK)
+        try:
+            device, _ = Device.objects.get_or_create(id=pk)
+            device.add_location(**request.data)
+            LOGGER.info(f"New update {request} Payload: {request.data}")
+            return Response("Update successful!", status=status.HTTP_200_OK)
+        except Exception as e:
+            LOGGER.error(f"Error in updating device: {e}")
+            return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     def destroy(self, request, pk=None):
-        device = get_object_or_404(Device, id=pk)
-        device.delete()
-        return Response("Device deleted successfully", status=status.HTTP_204_NO_CONTENT)
+        try:
+            device = get_object_or_404(Device, id=pk)
+            device.delete()
+            return Response("Device deleted successfully", status=status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            LOGGER.error(f"Error in deleting device: {e}")
+            return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @action(detail=True, methods=['GET'])
     def last_location(self, request, pk: int=None):
-        device = get_object_or_404(Device, id=pk)
-        serializer = self.serializer(device)
-        last_location = serializer.data.get('last_location')
-        return Response(last_location or "No location data available for this device.", status=status.HTTP_200_OK)
+        try:
+            device = get_object_or_404(Device, id=pk)
+            serializer = self.serializer(device)
+            last_location = serializer.data.get('last_location')
+            return Response(last_location or "No location data available for this device.", status=status.HTTP_200_OK)
+        except Exception as e:
+            LOGGER.error(f"Error in retrieving last location: {e}")
+            return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
